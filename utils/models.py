fed95c84b84d84b3656f2e66348d021b052431fa
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-01-03 18:10:51 +0400

    Add Text class

--- utils/models.py
+++ utils/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Text(models.Model):
+    PRODUCT = 'product'
+    SEARCH = 'search'
+    HOME = 'home'
+
+    TYPE_CHOICES = [
+        (PRODUCT, 'Product'),
+        (SEARCH, 'Search'),
+        (HOME, 'Home'),
+    ]
+
+    text = models.CharField(max_length=255)
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+
+
+class Banner(models):
+    image = models.ImageField()
+    company = models.CharField(max_length=255)
