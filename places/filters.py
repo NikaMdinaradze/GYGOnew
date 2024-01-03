240257532381e98c90a7b79efc0b51e67a49004e
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-01-03 17:15:09 +0400

    Add search for places

--- places/filters.py
+++ places/filters.py
@@ -1,11 +1,19 @@
 from django_filters import rest_framework as filters
 from places.models import Place
-
+from django.db import models
 
 class PlaceFilter(filters.FilterSet):
     min_price = filters.NumberFilter(field_name="main_price", lookup_expr='gte')
     max_price = filters.NumberFilter(field_name="main_price", lookup_expr='lte')
+    search = filters.CharFilter(method='custom_search_filter')
 
     class Meta:
         model = Place
-        fields = ("name", "category", "full_address")
+        fields = ("category", "district")
+
+    def custom_search_filter(self, queryset, name, value):
+        return queryset.filter(
+            models.Q(name__icontains=value) |
+            models.Q(full_address__icontains=value) |
+            models.Q(category__icontains=value)
+        )
