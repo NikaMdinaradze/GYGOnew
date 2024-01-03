8ea3cc8b2c33c5f556429b9679edb2b5a820910a
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-01-03 18:14:56 +0400

    Add Banner serializers

---
+++ utils/serializers.py
@@ -0,0 +1,9 @@
+from utils.models import Text
+from rest_framework import serializers
+
+
+class TextSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Text
+        fields = "__all__"
+
