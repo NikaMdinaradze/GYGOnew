71c2b60f5b1ed3bcba90a41ab7690afbf53e7ae2
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-01-03 23:28:11 +0400

    Add filter for text

--- utils/views.py
+++ utils/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from utils.serializers import TextSerializer, BannerSerializer
 from utils.models import Text, Banner
+from django_filters import rest_framework as filters
+from utils.filters import TextFilter
 # Create your views here.
 
 
@@ -8,6 +10,8 @@ class TextListView(generics.ListAPIView):
     serializer_class = TextSerializer
     queryset = Text.objects.all()
     ordering = ['id']
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = TextFilter
 
 
 class BannerListView(generics.ListAPIView):
