e29d8f1ef51708d5673bb58db429ff88ca47535d
NikaMdinaradze <mdinaradzenika04@gmail.com>
2024-02-10 15:37:04 +0400

    Add allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -25,11 +25,15 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost",
                  "https://gygo.vercel.app",
-                 "https://nikamdinaradze.com",]
+                 "https://nikamdinaradze.com",
+                 "gygo.vercel.app",
+                 "www.gygo.vercel.app",
+                 "nikamdinaradze.com",
+                 "www.nikamdinaradze.com"]
 
 # cors configuration
 
